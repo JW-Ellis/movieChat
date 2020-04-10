7aa5d96cd36afb7dad5ff87a764662946d3d0480
JW-Ellis <jwilliamellis@gmail.com>
2020-04-10 18:32:43 -0500

    add join and chat components add join styling

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
+
+const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          {/* Sets name of user */}
+          <input
+            placeholder=""
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          {/* Sets chatroom name */}
+          <input
+            placeholder=""
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          // If name and chat room params provided redirect to chat endpoint otherwise do not redirect
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connection", (socket) => {
+  console.log("New connection");
+
+  socket.on("disconnect", () => {
+    console.log("User left");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
