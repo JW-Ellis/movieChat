c870b23c10b58d010c6efb74c4bb81140fb5bb3f
JW-Ellis <jwilliamellis@gmail.com>
2020-04-12 20:16:49 -0500

    Add infobar and input components

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState("");
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -25,7 +30,37 @@ const Chat = ({ location }) => {
     };
   }, [ENDPOINT, location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    //adds new messages to message array
+    socket.on("message", (message) => {
+      setMessages({ ...messages, message });
+    });
+  }, [messages]);
+
+  //function that sends messages
+  const sendMessage = (event) => {
+    //prevents browser page from refreshing
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
