52e7268530d6d88e1f337f4718616704b4f709de
JW-Ellis <jwilliamellis@gmail.com>
2020-05-09 00:40:10 -0500

    add login links to posters

--- client/src/components/Movie/Movie.js
+++ client/src/components/Movie/Movie.js
@@ -6,7 +6,9 @@ import "./Movie.css";
 const Movie = () => {
   return (
     <div className="movieOuterContainer">
-      <h1>test</h1>
+      <div>
+        <h1>test</h1>
+      </div>
       <section className="moviePosters" id="moviePosters">
         <ul>
           <li>
@@ -22,42 +24,90 @@ const Movie = () => {
           </li>
           <li>
             <figure>
-              <img src="../img/gonegirl.jpg" alt="Gone Girl movie Promo"></img>
+              <Link to={`/join?room=gonegirl`}>
+                <img
+                  className="poster"
+                  src="../img/gonegirl.jpg"
+                  alt="Gone Girl movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/joker.jpg" alt="Joker movie Promo"></img>
+              <Link to={`/join?room=joker`}>
+                <img
+                  className="poster"
+                  src="../img/joker.jpg"
+                  alt="Joker movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/midsommar.jpg" alt="Midsommar movie Promo"></img>
+              <Link to={`/join?room=midsommar`}>
+                <img
+                  className="poster"
+                  src="../img/midsommar.jpg"
+                  alt="Midsommar movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/parasite.jpg" alt="Parasite movie Promo"></img>
+              <Link to={`/join?room=parasite`}>
+                <img
+                  className="poster"
+                  src="../img/parasite.jpg"
+                  alt="Parasite movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/prestige.jpg" alt="Prestige movie Promo"></img>
+              <Link to={`/join?room=prestige`}>
+                <img
+                  className="poster"
+                  src="../img/prestige.jpg"
+                  alt="Prestige movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/psycho.jpg" alt="Psycho movie Promo"></img>
+              <Link to={`/join?room=psycho`}>
+                <img
+                  className="poster"
+                  src="../img/psycho.jpg"
+                  alt="Psycho movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/starwars.jpg" alt="Starwars movie Promo"></img>
+              <Link to={`/join?room=starwars`}>
+                <img
+                  className="poster"
+                  src="../img/starwars.jpg"
+                  alt="Starwars movie Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
           <li>
             <figure>
-              <img src="../img/tigerking.jpg" alt="Tiger King show Promo"></img>
+              <Link to={`/join?room=tigerking`}>
+                <img
+                  className="poster"
+                  src="../img/tigerking.jpg"
+                  alt="Tiger King show Promo"
+                ></img>
+              </Link>
             </figure>
           </li>
         </ul>
