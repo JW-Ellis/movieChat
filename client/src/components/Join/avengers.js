4dd98fd94bbe4481e6e609ed37ca57a39ce6a3f8
JW-Ellis <jwilliamellis@gmail.com>
2020-07-03 23:22:02 -0500

    update admit page styling

--- client/src/components/Join/avengers.js
+++ client/src/components/Join/avengers.js
@@ -13,38 +13,45 @@ const Avengers = () => {
 
   return (
     <div className="joinOuterContainer">
-      <div className="posterContainer">
-        <figure>
-          <img
-            className="joinPoster"
-            src="../img/avengers.jpg"
-            alt="Avengers movie promo"
-          />
-        </figure>
+      <div className="secondaryHeader">
+        <div className="secondaryImg"></div>
       </div>
-      <div className="joinInnerContainer">
-        <div className="joinTitle">
-          <h1>Sign in! </h1>
+      <div className="signIn">
+        <div className="posterContainer">
+          <figure>
+            <img
+              className="joinPoster"
+              src="../img/avengers.jpg"
+              alt="Avengers movie promo"
+            />
+          </figure>
         </div>
-        <div>
-          <p></p>
-          {/* Sets chatroom name */}
-          <input
-            placeholder="User Name"
-            className="joinInput mt-20"
-            type="text"
-            onChange={(event) => setName(event.target.value)}
-          />
+        <div className="joinInnerContainer">
+          <div className="joinTitle">
+            <div className="admit"></div>
+          </div>
+          <div>
+            <p></p>
+            {/* Sets chatroom name */}
+            <input
+              placeholder="User Name"
+              className="joinInput mt-20"
+              type="text"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <Link
+            // If name and chat room params provided redirect to chat endpoint otherwise do not redirect
+            onClick={(event) =>
+              !name || !room ? event.preventDefault() : null
+            }
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className="button mt-20" type="submit">
+              Enter
+            </button>
+          </Link>
         </div>
-        <Link
-          // If name and chat room params provided redirect to chat endpoint otherwise do not redirect
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className="button mt-20" type="submit">
-            Submit
-          </button>
-        </Link>
       </div>
     </div>
   );
--- client/src/components/Join/starwars.js
+++ client/src/components/Join/starwars.js
@@ -12,38 +12,45 @@ const StarWars = () => {
   }, []);
   return (
     <div className="joinOuterContainer">
-      <div className="posterContainer">
-        <figure>
-          <img
-            className="joinPoster"
-            src="../img/starwars.jpg"
-            alt="StarWars movie promo"
-          />
-        </figure>
+      <div className="secondaryHeader">
+        <div className="secondaryImg"></div>
       </div>
-      <div className="joinInnerContainer">
-        <div className="joinTitle">
-          <h1>Sign in! </h1>
+      <div className="signIn">
+        <div className="posterContainer">
+          <figure>
+            <img
+              className="joinPoster"
+              src="../img/starwars.jpg"
+              alt="StarWars movie promo"
+            />
+          </figure>
         </div>
-        <div>
-          <p></p>
-          {/* Sets chatroom name */}
-          <input
-            placeholder="User Name"
-            className="joinInput mt-20"
-            type="text"
-            onChange={(event) => setName(event.target.value)}
-          />
+        <div className="joinInnerContainer">
+          <div className="joinTitle">
+            <div className="admit"></div>
+          </div>
+          <div>
+            <p></p>
+            {/* Sets chatroom name */}
+            <input
+              placeholder="User Name"
+              className="joinInput mt-20"
+              type="text"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <Link
+            // If name and chat room params provided redirect to chat endpoint otherwise do not redirect
+            onClick={(event) =>
+              !name || !room ? event.preventDefault() : null
+            }
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className="button mt-20" type="submit">
+              Submit
+            </button>
+          </Link>
         </div>
-        <Link
-          // If name and chat room params provided redirect to chat endpoint otherwise do not redirect
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className="button mt-20" type="submit">
-            Submit
-          </button>
-        </Link>
       </div>
     </div>
   );
