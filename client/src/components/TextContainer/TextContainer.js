9b3341c03384d4e62cb261861cf5b9e002e5095a
JW-Ellis <jwilliamellis@gmail.com>
2021-01-16 11:09:25 -0600

    remove unused component and image reference

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
-import onlineIcon from "../../Icons/onlineIcon.png";
+
 
 import "./TextContainer.css";
 
